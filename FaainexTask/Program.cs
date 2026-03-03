1316d552f82377073d92ff34245dafd62cac6f4e
samaateff <71303025+samaateff@users.noreply.github.com>
2026-03-03 03:58:49 +0200

    first commit

---
+++ FaainexTask/Program.cs
@@ -0,0 +1,38 @@
+using FaainexTask.Client.Pages;
+using FaainexTask.Components;
+using MudBlazor.Services;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(FaainexTask.Client._Imports).Assembly);
+
+app.Run();
